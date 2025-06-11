0c7d0ff0ed88342da3edd0327cbc1e9ee0f5fd49
mulham-salem <smulhams@gmail.com>
2025-06-11 23:47:15 +0300

    Merged remote changes with local backend work 2

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -10,13 +10,13 @@ use App\Http\Controllers\SupplyManagementController;
 use App\Http\Controllers\TableManagementController;
 use App\Http\Controllers\UserAuthController;
 use App\Http\Controllers\UserManagementController;
-use App\Models\Notification;
 use App\Http\Controllers\TableReservationController;
 use Illuminate\Support\Facades\Route;
 
 // .......................................................User Routes.........................................................
 
 Route::post('/user/login', [UserAuthController::class, 'login'])->middleware('throttle:3,1');
+
 Route::middleware('auth:sanctum')->prefix('user')->group(function () {
     Route::get('/profile', [UserAuthController::class, 'profile']);
     Route::post('/logout', [UserAuthController::class, 'logout']);
@@ -26,6 +26,7 @@ Route::middleware('auth:sanctum')->prefix('user')->group(function () {
 // ......................................................Manager Access......................................................
 
 Route::post('/manager/login', [ManagerAuthController::class, 'login'])->middleware('throttle:3,1');
+
 Route::middleware(['auth:manager', 'isManager'])->prefix('manager')->group(function () {
     Route::get('/profile', [ManagerAuthController::class, 'profile']);
     Route::post('/logout', [ManagerAuthController::class, 'logout']);
@@ -40,32 +41,25 @@ Route::middleware(['auth:manager', 'isManager'])->prefix('manager')->group(funct
     Route::post('/supply-offers/{id}/accept', [SupplyManagementController::class, 'acceptOffer']);
     Route::post('/supply-offers/{id}/reject', [SupplyManagementController::class, 'rejectOffer']);
     Route::post('/supply-purchase-bill', [SupplyManagementController::class, 'storePurchaseBill']);
+
+    //** notification **//
+    Route::get('/notification', [NotificationManagementController::class, 'index']);//supplyOffersNotification
+    Route::get('/notification2', [NotificationManagementController::class, 'index5']);//getResponseFromSupplierForManagerSupplyRequests
 });
+
 // ......................................................Supplier Access ......................................................
 
 Route::middleware(['auth:sanctum', 'checkUserRole:supplier'])->group(function () {
     Route::post('/supplier/offers', [SupplierController::class, 'store']);
     Route::get('/supplier/view-offers', [SupplierController::class, 'viewMyOffers']);
-    Route::get('/supplier/notification', [Notification::class, '']);
-=======
-    Route::post('/supply-purchace-bill', [SupplyManagementController::class, 'storePurchaseBill']);
-    // notification
-    Route::get('/notification', [NotificationManagementController::class, 'index']);//supplyOffersNotificatoin
-        Route::get('/notification2', [NotificationManagementController::class, 'index5']);//getResponseFromSupplierForManagerSupplyRequests
-});
 
-// .......................Supplier Access ..........................................................................................
-
-Route::middleware(['auth:sanctum', 'checkUserRole:supplier'])->group(function () {
-    Route::post('/supplier/offers', [SupplierController::class, 'store']);
-    Route::get('/supplier/view-offers', [SupplierController::class, 'viewMyOffers']);
-    // notification
+    //** notification **//
     Route::get('/supplier/notification1', [NotificationManagementController::class, 'index2']);//responseFromManagerForMyFuckingOffers!!
     Route::get('/supplier/notification2', [NotificationManagementController::class, 'index3']);//getSupplyRequestsForSupplier
     Route::get('/supplier/notification3/{id}', [NotificationManagementController::class, 'index4']);//respondToSupplyRequest
 });
 
-// ......................................................customer Routes.....................................................
+// ....................................................customer Routes.....................................................
 
 Route::middleware(['auth:sanctum', 'checkUserRole:customer'])->prefix('user/customer')->group(function () {
     Route::get('/table-reservation/available', [TableReservationController::class, 'indexAvailableTables']);
